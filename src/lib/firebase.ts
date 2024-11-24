ae289820ff6eaa6c6e13062dda25014e8e68cc32
ia-nocode <ia.nocode.fr@gmail.com>
2024-11-24 20:12:22 +0100

    Update firebase.ts

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -3,12 +3,12 @@ import { getAuth } from 'firebase/auth';
 import { getFirestore } from 'firebase/firestore';
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBDamoj1DvJGRjvVgzVfiafS6ywuxp5sCs",
-  authDomain: "roles-1d1c1.firebaseapp.com",
-  projectId: "roles-1d1c1",
-  storageBucket: "roles-1d1c1.firebasestorage.app",
-  messagingSenderId: "1081823746875",
-  appId: "1:1081823746875:web:352f4f1a39b31d184c751e"
+  apiKey: "XXX",
+  authDomain: "XXX",
+  projectId: "XXX",
+  storageBucket: "XXX",
+  messagingSenderId: "XXX",
+  appId: "XXX"
 };
 
 export const app = initializeApp(firebaseConfig);
